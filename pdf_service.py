33601e6a4b0ec8afdb9849336c59828aa9b717e1
Vijayraj Kamat <vijayraj.kamat@gmail.com>
2026-05-17 00:28:14 +0530

    Create generated_pdfs folder automatically

--- pdf_service.py
+++ pdf_service.py
@@ -7,6 +7,9 @@ import os
 
 
 def generate_pdf(data, submission_id):
+    output_dir = "generated_pdfs"
+
+    os.makedirs(output_dir, exist_ok=True)
 
     env = Environment(
         loader=FileSystemLoader("templates")
@@ -19,37 +22,31 @@ def generate_pdf(data, submission_id):
     )
 
     html_content = template.render(
-
         name=data.name,
-
         purpose=data.purpose,
-
         strengths=data.strengths,
-
         skills=data.skills,
-
         values=data.values,
-
         passions=data.passions,
-
         background_image=background_image
     )
 
-    temp_html_path = f"generated_pdfs/temp_{submission_id}.html"
+    temp_html_path = os.path.join(
+        output_dir,
+        f"temp_{submission_id}.html"
+    )
 
     with open(temp_html_path, "w", encoding="utf-8") as file:
-
         file.write(html_content)
 
     safe_name = data.name.strip().replace(" ", "_")
-    
-    output_pdf_path = (
-        f"generated_pdfs/"
+
+    output_pdf_path = os.path.join(
+        output_dir,
         f"{submission_id}_Purpose_card_{safe_name}.pdf"
-	)
+    )
 
     with sync_playwright() as p:
-
         browser = p.chromium.launch()
 
         page = browser.new_page()
@@ -59,17 +56,17 @@ def generate_pdf(data, submission_id):
         )
 
         page.pdf(
-	    path=output_pdf_path,
-	    format="A4",
-	    landscape=True,
-	    print_background=True,
-	    margin={
-	        "top": "0mm",
-	        "right": "0mm",
-	        "bottom": "0mm",
-	        "left": "0mm"
-	    }
-	)
+            path=output_pdf_path,
+            format="A4",
+            landscape=True,
+            print_background=True,
+            margin={
+                "top": "0mm",
+                "right": "0mm",
+                "bottom": "0mm",
+                "left": "0mm"
+            }
+        )
 
         browser.close()
 
